0e0e3682148a4e278ca3d18043dca32d7188d7e3
amirhosein <amirhoseinhoseinpor@gmail.com>
2023-04-16 00:25:30 +0430

    Add project files.

---
+++ tamrin 8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+class Currency
+{
+    static void ReadAPI(string[] args)
+    {
+        using var client = new WebClient();
+        var content = client.DownloadString("https://api.kucoin.com/api/v1/market/stats?symbol=btc-USDT");
+        StreamWriter writer = new StreamWriter(content);
+        writer.WriteLine(content);
+        Console.WriteLine(content);
+    }
+
+}
